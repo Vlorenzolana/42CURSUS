613088a3f13ee0c32c310b7154d606554ae86a67
vlorenzo <vlorenzolana@gmail.com>
2025-02-19 17:04:19 +0100

    Test OK

--- ExamRank03/ft_printf/ft_printf.c
+++ ExamRank03/ft_printf/ft_printf.c
@@ -2,42 +2,35 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int	ft_putnbr(int n, int base)
-{
-	char	digit[17] = "0123456789abcdef";
-	int		count;
-
-	count = 0;
-	if (n > (base - 1))
-		ft_putnbr(n / base, base);
-	count += write(1, &digit[n % base], 1);
-	return (count);
+void	ft_putnbr(int n, int base, int *count)  // We call count by ref. &count
+{												//
+	char digit[17] = "0123456789abcdef";		// Due to recursivity, issue with regular counters.		
+	if (n > (base - 1))							//
+		ft_putnbr(n / base, base, count);    	// But if we call it by ref. within the function
+	*count += write(1, &digit[n % base], 1); 	// We only need a pointer to pass it on
 }
 
-int	ft_printf(const char *fmt, ...)
+int	ft_printf(char *fmt, ...)
 {
 	va_list	ap;
+	int		count;
 	int		d;
 	int		x;
 	char	*s;
-	int		count;
 
-	count = 0;
 	va_start(ap, fmt);
+	count = -1;
 	while (*fmt)
 	{
 		if (fmt && *fmt == '%')
 		{
-			count++;
+			++count;
 			fmt++;
 			if (*fmt == 's')
 			{
 				s = va_arg(ap, char *);
 				if (s == NULL)
-				{
 					count += write(1, "(null)", 6);
-					count--;
-				}
 				else
 					while (s && *s)
 						count += write(1, s++, 1);
@@ -45,12 +38,12 @@ int	ft_printf(const char *fmt, ...)
 			else if (*fmt == 'd')
 			{
 				d = va_arg(ap, int);
-				count += ft_putnbr(d, 10);
+				ft_putnbr(d, 10, &count); // We call count by ref. &count
 			}
 			else if (*fmt == 'x')
 			{
 				x = va_arg(ap, int);
-				count += ft_putnbr(x, 16);
+				ft_putnbr(x, 16, &count); // We call count by ref. &count
 			}
 			else
 			{
@@ -69,27 +62,49 @@ int	ft_printf(const char *fmt, ...)
 int	main(void)
 {
 	char	*ptr;
+	char	*c;
 
+	c = NULL;
 	ptr = NULL;
+	ft_printf("MIARD %d\n", ft_printf("%s ", c));
+	printf("STDRD %d\n", printf("%s ", c));
+	ft_printf("\n");
 	ft_printf("%d\n", ft_printf("%s ", ptr));
 	printf("%d\n", printf("%s ", ptr));
-	ft_printf("%d\n", ft_printf("%d ", 52)); // count = 3 -> 5; 2; \n;
-	printf("%d\n", printf("%d ", 52));       // count = 3 -> 5; 2; \n;
+	ft_printf("\n");
+	ft_printf("MIARD %d\n", ft_printf("%d ", 5265476));
+	printf("STDRD %d\n", printf("%d ", 5265476));
+	ft_printf("\n");
+	sleep(1 / 2);
+	ft_printf("MIARD %d\n", ft_printf("%x ", 33));
+	printf("STDRD %d\n", printf("%x ", 33));
+	sleep(1 / 2);
+	ft_printf("\n");
 	ft_printf("%x\n", 9);
 	printf("%x\n", 9);
+	ft_printf("\n");
 	ft_printf("%s\n", "");
 	printf("%s\n", "");
-	ft_printf("Magic %s is %d", NULL, 42);
-	printf("Magic %s is %d", "", 42);
+	ft_printf("\n");
+	ft_printf("Magic %s is %d\n", NULL, 42);
+	printf("Magic %s is %d\n", "", 42);
+	ft_printf("\n");
 	ft_printf("Hexadecimal for %d is %x\n", '0', 42);
-	ft_printf("%s\n", "I love watermelons");
+	ft_printf("%d\n", ft_printf("%s\n", "I love watermelons"));
+	printf("%d\n", printf("%s\n", "I love watermelons"));
+	ft_printf("\n");
 	ft_printf("%p\n", 42);
 	ft_printf("%s\n", NULL);
 	ft_printf("");
 	ft_printf("%%\n");
-	ft_printf("Magic %s is %d\n", "number", 31);
+	ft_printf("\n");
+	ft_printf("Magic %s is %d\n", "number", 42);
+	printf("Magic %s is %d\n", "number", 42);
+	ft_printf("\n");
 	ft_printf("Hexadecimal for %d is %x\n", 42, 42);
-	/* 	ft_printf("%d\n", ft_printf("ft = *+Ç!=?¿!"));
+	printf("Magic %s is %d\n", "number", 42);
+	ft_printf("\n");
+	/*  ft_printf("%d\n", ft_printf("ft = *+Ç!=?¿!"));
 		// OJO!!
 		// los caracteres especiales los BUFFERiza de uno en uno y tarda la hostia
 		// se mezclan los bufferes de un printf anterior que no haya terminado
