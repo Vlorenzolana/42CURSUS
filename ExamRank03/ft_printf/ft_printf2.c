bfc6e0ace6c7819f0bbcba0d86c1d20f84383b6f
vlorenzo <vlorenzolana@gmail.com>
2025-03-03 09:38:11 +0100

    Workinprogress test DataRaces

--- ExamRank03/ft_printf/ft_printf2.c
+++ ExamRank03/ft_printf/ft_printf2.c
@@ -3,71 +3,133 @@
 #include <stdio.h>
 #include <unistd.h>
 
-void	ft_putnbr(long n, int base, int *count) // We call count by ref. &count
+void	ft_putchar(char c, int *counter)
 {
-	char digit[17] = "0123456789abcdef";
-	if (n < 0)
-	{
-		count += write(1, "-", 1);
-		n *= -1;
-	}
-	if (n >= (base))
-		ft_putnbr(n / base, base, count);
-	*count += write(1, &digit[n % base], 1);
+	counter += write(1, &c, 1);
 }
 
-int	ft_printf(char *fmt, ...)
+void	ft_putstr(char *str, int *counter)
 {
-	va_list	ap;
-	int		count;
-	char	*s;
-	long	x;
+	int	i;
 
-	va_start(ap, fmt);
-	count = -1;
-	while (*fmt)
+	i = 0;
+	while (str[i] != '\0')
 	{
-		if (fmt && *fmt == '%')
+		*counter += write(1, &str[i], 1);
+		i++;
+	}
+}
+
+void	ft_putnbr(long n, int *counter)
+{
+	int	result;
+
+	if (n < INT_MIN || n > INT_MAX)
+		return ;
+	if (n < 0)
+	{
+		*counter += write(1, "-", 1);
+		n *= -1;
+	}
+	if (n < 10)
+	{
+		result = n + '0';
+		*counter += write(1, &result, 1);
+	}
+	else
+	{
+		result = (n % 10) + '0';
+		ft_putnbr(n / 10, counter);
+		*counter += write(1, &result, 1);
+	}
+}
+
+void	ft_puthex(long n, int *counter)
+{
+	char	*alpha;
+	char	result;
+
+	alpha = "abcedf";
+	if (n < 0)
+	{
+		*counter += write(1, "-", 1);
+		n *= -1;
+	}
+	if (n < 16)
+	{
+		if (n <= 9)
 		{
-			++count;
-			fmt++;
-			if (*fmt == 's')
-			{
-				s = va_arg(ap, char *);
-				if (s == NULL)
-					count += write(1, "(null)", 6);
-				else
-					while (s && *s)
-						count += write(1, s++, 1);
-			}
-			else if (*fmt == 'd')
-				ft_putnbr((va_arg(ap, int)), 10, &count);
-			else if (*fmt == 'x')
-			{
-				x = va_arg(ap, unsigned long int);
-				printf("%ld   ", x);
-				ft_putnbr(x, 16, &count);
-			}
-			else
-			{
-				count += write(1, "%", 1);
-				count += write(1, fmt, 1);
-			}
+			result = n + '0';
+			*counter += write(1, &result, 1);
 		}
 		else
-			count += write(1, fmt, 1);
-		fmt++;
+		{
+			result = alpha[n - 9 - 1];
+			*counter += write(1, &result, 1);
+		}
 	}
-	va_end(ap);
-	return (count);
+	else
+	{
+		ft_puthex(n / 16, counter);
+		if (n % 16 < 10)
+		{
+			result = n + '0';
+			*counter += write(1, &result, 1);
+		}
+		else
+		{
+			result = alpha[(n % 16) - 9 - 1];
+			*counter += write(1, &result, 1);
+		}
+	}
+}
+
+void	ft_flags(char c, va_list args, int *counter)
+{
+	if (c == 'c')
+		ft_putchar(va_arg(args, int), counter);
+	else if (c == 's')
+		ft_putstr(va_arg(args, char *), counter);
+	else if (c == 'd')
+		ft_putnbr(va_arg(args, int), counter);
+	else if (c == 'x')
+		ft_puthex(va_arg(args, int), counter);
+}
+
+int	ft_printf(const char *s, ...)
+{
+	va_list	args;
+	int		i;
+	int		counter;
+
+	va_start(args, s);
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == '%')
+		{
+			if (s[i + 1] == '%')
+				counter += write(1, "%", 1);
+			else
+				ft_flags(s[i + 1], args, &counter);
+			i += 2;
+		}
+		else
+		{
+			counter += write(1, &s[i], 1);
+			i++;
+		}
+	}
+	va_end(args);
+	return (1);
 }
 
 #include <limits.h>
 
 int	main(void)
 {
-	char	*ptr;
-	char	*c;
+	char *ptr;
+	char *c;
 
 	c = NULL;
 	ptr = NULL;
@@ -80,11 +142,11 @@ int	main(void)
 	ft_printf("\n");
 	ft_printf("\n");
 	ft_printf("FT  Hexadecimal for %d is %x\n", 2147483647, 2147483647);
-	ft_printf("FT  Hexadecimal for %d is %x\n", -2147483648, -2147483648);
+	ft_printf("FT  Hexadecimal for %d is %x\n", -2147483647, -2147483647);
 	ft_printf("\n");
 	ft_printf("\n");
 	printf("STD  Hexadecimal for %d is %x\n", 2147483647, 2147483647);
-	printf("STD  Hexadecimal for %ld is %lx\n", -2147483647, -2147483647);
+	printf("STD  Hexadecimal for %d is %x\n", -2147483647, -2147483647);
 	/* 	ft_printf("MIARD %d\n", ft_printf("%s ", c));
 		printf("STDRD %d\n", printf("%s ", c));
 		ft_printf("\n");
